8e0f8e271f163649dab0a631ba4e15bd8e5476b9
Porf'irio Ribeiro <porfirioribeiro@gmail.com>
2020-11-30 22:50:10 +0000

    no need for copy snowpack config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,33 +1,11 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-  install: ["svelte"],
-  installOptions: {
-    // ignore `import fs from 'fs'` etc
-    externalPackage: require("module").builtinModules,
-  },
-  plugins: [
-    [
-      "@snowpack/plugin-svelte",
-      {
-        compilerOptions: {
-          hydratable: true,
-        },
-      },
-    ],
-    "@snowpack/plugin-typescript",
-  ],
-  devOptions: {
-    open: "none",
-  },
-  buildOptions: {
-    sourceMaps: true,
-  },
+  extends: "@sveltejs/snowpack-config",
+  plugins: ["@snowpack/plugin-typescript"],
   mount: {
-    ".svelte/assets": "/_app/assets",
-    "src": "/_src",
+    src: "/_src",
   },
   alias: {
-    $app: "./.svelte/assets/app",
     $components: "./src/components",
   },
 };
