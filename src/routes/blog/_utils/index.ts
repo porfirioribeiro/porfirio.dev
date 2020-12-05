81a43a8aab0bcf7621e74a0979ad5e945afa551e
Porf'irio Ribeiro <porfirioribeiro@gmail.com>
2020-12-05 21:09:00 +0000

    change fs promises

--- src/routes/blog/_utils/index.ts
+++ src/routes/blog/_utils/index.ts
@@ -1,4 +1,4 @@
-import fs from "fs/promises";
+import { promises as fs } from "fs";
 import path from "path";
 import frontMatter, { FrontMatterResult } from "front-matter";
 import formatDate from "date-fns/format";
