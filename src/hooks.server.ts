466db5abcc6bc9c6f45d606908c33a88db2e0c8b
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2022-12-30 11:43:15 +0000

    max age

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import type { Handle, HandleFetch } from "@sveltejs/kit";
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
 
-  response.headers.set("Cache-Control", "public, max-age=30, s-maxage=86400");
+  response.headers.set("Cache-Control", "public, max-age=5, s-maxage=86400");
 
   console.log("handle", event.url.pathname);
 
