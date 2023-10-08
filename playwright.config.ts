daba09dcbf777d2ea7a8bacf987add395a21f4e9
Porfirio <porfirioribeiro@gmail.com>
2023-10-08 18:11:03 +0100

    chore: tests (#25)
    * chore: disable webkit
    
    * test status
    
    * test on prod

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173',
+		baseURL: 'https://porfirio.dev',
+		// baseURL: 'http://localhost:5173',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry'
@@ -42,20 +43,20 @@ export default defineConfig({
 			use: { ...devices['Desktop Firefox'] }
 		},
 
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] }
-		},
+		// {
+		// 	name: 'webkit',
+		// 	use: { ...devices['Desktop Safari'] }
+		// },
 
 		/* Test against mobile viewports. */
 		{
 			name: 'Mobile Chrome',
 			use: { ...devices['Pixel 5'] }
-		},
-		{
-			name: 'Mobile Safari',
-			use: { ...devices['iPhone 12'] }
 		}
+		// {
+		// 	name: 'Mobile Safari',
+		// 	use: { ...devices['iPhone 12'] }
+		// }
 
 		/* Test against branded browsers. */
 		// {
