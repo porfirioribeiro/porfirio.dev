51eeb517936bd863f84aa6705eed9bf9bda7c2fa
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2024-10-20 21:12:53 +0100

    Fix sass api warning

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 import cloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({})],
+	preprocess: [sveltePreprocess({})],
 
 	kit: {
 		adapter: cloudflare()
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: { preprocessorOptions: { scss: { api: 'modern-compiler' } } }
 };
 
 export default config;
